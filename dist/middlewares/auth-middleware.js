c9c1609b17165b84ae32a803b7d2414b14211be7
yegor25 <lesnichij94@mail.ru>
2023-09-25 02:28:10 +0300

    hw2 errors with auth

--- dist/middlewares/auth-middleware.js
+++ dist/middlewares/auth-middleware.js
@@ -7,18 +7,32 @@ const checkAuth = (req, res, next) => {
         res.sendStatus(401);
         return;
     }
-    const encode = atob(user === null || user === void 0 ? void 0 : user.split(" ").splice(1, 1).join(" "));
-    const encodeArray = encode.split(":");
-    if (encodeArray.length !== 2) {
-        res.sendStatus(401);
-        return;
-    }
-    if (encodeArray[0] === "admin" && encodeArray[1] === "qwerty") {
-        return next();
+    const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    const isBase64 = base64regex.test(user);
+    if (isBase64) {
+        const encode = atob(user === null || user === void 0 ? void 0 : user.split(" ").splice(1, 1).join(" "));
+        const encodeArray = encode.split(":");
+        if (encodeArray.length !== 2) {
+            res.sendStatus(401);
+            return;
+        }
+        if (encodeArray[0] === "admin" && encodeArray[1] === "qwerty") {
+            return next();
+        }
+        else {
+            res.sendStatus(401);
+            return;
+        }
     }
     else {
-        res.sendStatus(401);
-        return;
+        const data = user === null || user === void 0 ? void 0 : user.split(" ").splice(1, 1);
+        if (data[0] === "admin" && data[1] === "qwerty") {
+            return next();
+        }
+        else {
+            res.sendStatus(401);
+            return;
+        }
     }
 };
 exports.checkAuth = checkAuth;
