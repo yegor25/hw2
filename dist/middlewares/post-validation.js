0056e1eb15ad7cffe168d7d43ce7419fa38e8542
yegor25 <lesnichij94@mail.ru>
2023-09-25 02:53:57 +0300

    hw2 errors custom

--- dist/middlewares/post-validation.js
+++ dist/middlewares/post-validation.js
@@ -2,11 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.postValidate = exports.postValidator = void 0;
 const express_validator_1 = require("express-validator");
+const blog_repository_1 = require("../repositories/blog-repository");
 exports.postValidator = [
     (0, express_validator_1.body)("title").exists().isString().notEmpty().trim().isLength({ min: 3, max: 30 }).withMessage("invalid title"),
     (0, express_validator_1.body)("shortDescription").trim().notEmpty().isString().isLength({ min: 3, max: 100 }).withMessage("invalid short description"),
     (0, express_validator_1.body)("content").trim().notEmpty().isString().isLength({ min: 3, max: 1000 }).withMessage("invalid content"),
-    (0, express_validator_1.body)("blogId").trim().isString().notEmpty().withMessage("required valid blogId")
+    (0, express_validator_1.body)("blogId").exists().trim().isString().notEmpty().withMessage("required valid blogId"),
+    (0, express_validator_1.body)("blogId").custom(val => blog_repository_1.blogsRepository.findBlogById(val))
 ];
 const postValidate = (req, res, next) => {
     const errorFormatter = ({ msg, path }) => {
--- src/middlewares/post-validation.ts
+++ src/middlewares/post-validation.ts
@@ -4,12 +4,14 @@ import {  Response, NextFunction } from "express"
 import { requestWithBody } from "../types/root-type"
 import {  body, validationResult } from "express-validator"
 import { postBodyType } from "../types/post-type"
+import { blogsRepository } from "../repositories/blog-repository"
 
 export const postValidator =  [
     body("title").exists().isString().notEmpty().trim().isLength({min: 3,max: 30}).withMessage("invalid title"),
     body("shortDescription").trim().notEmpty().isString().isLength({min: 3,max: 100}).withMessage("invalid short description"),
     body("content").trim().notEmpty().isString().isLength({min: 3,max: 1000}).withMessage("invalid content"),
-    body("blogId").trim().isString().notEmpty().withMessage("required valid blogId")
+    body("blogId").exists().trim().isString().notEmpty().withMessage("required valid blogId"),
+    body("blogId").custom( val => blogsRepository.findBlogById(val))
     
 ]
 
