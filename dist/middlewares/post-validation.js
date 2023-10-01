94cd2ecfc057f83f035f52b79955ae168208ba24
yegor25 <lesnichij94@mail.ru>
2023-10-01 04:30:15 +0300

    with remove all datssssa

--- dist/middlewares/post-validation.js
+++ dist/middlewares/post-validation.js
@@ -7,8 +7,7 @@ exports.postValidator = [
     (0, express_validator_1.body)("title").exists().isString().notEmpty().trim().isLength({ min: 3, max: 30 }).withMessage("invalid title"),
     (0, express_validator_1.body)("shortDescription").trim().notEmpty().isString().isLength({ min: 3, max: 100 }).withMessage("invalid short description"),
     (0, express_validator_1.body)("content").trim().notEmpty().isString().isLength({ min: 3, max: 1000 }).withMessage("invalid content"),
-    (0, express_validator_1.body)("blogId").exists().trim().isString().notEmpty().withMessage("required valid blogId"),
-    (0, express_validator_1.body)("blogId").custom((val) => mongodb_1.ObjectId.isValid(val))
+    (0, express_validator_1.body)("blogId").exists().trim().isString().notEmpty().custom((val) => mongodb_1.ObjectId.isValid(val)).withMessage("required valid blogId"),
 ];
 const postValidate = (req, res, next) => {
     const errorFormatter = ({ msg, path }) => {
--- src/middlewares/post-validation.ts
+++ src/middlewares/post-validation.ts
@@ -11,8 +11,8 @@ export const postValidator =  [
     body("title").exists().isString().notEmpty().trim().isLength({min: 3,max: 30}).withMessage("invalid title"),
     body("shortDescription").trim().notEmpty().isString().isLength({min: 3,max: 100}).withMessage("invalid short description"),
     body("content").trim().notEmpty().isString().isLength({min: 3,max: 1000}).withMessage("invalid content"),
-    body("blogId").exists().trim().isString().notEmpty().withMessage("required valid blogId"),
-    body("blogId").custom(  (val) => ObjectId.isValid(val))
+    body("blogId").exists().trim().isString().notEmpty().custom((val) => ObjectId.isValid(val)).withMessage("required valid blogId"),
+    
     
 ]
 
