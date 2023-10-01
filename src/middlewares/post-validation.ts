9a637ac9c86642cb5532a6afec846e7545bb0365
yegor25 <lesnichij94@mail.ru>
2023-10-01 03:57:43 +0300

    with blogid

--- src/middlewares/post-validation.ts
+++ src/middlewares/post-validation.ts
@@ -5,13 +5,14 @@ import { requestWithBody } from "../types/root-type"
 import {  body, validationResult } from "express-validator"
 import { postBodyType } from "../types/post-type"
 import { blogsRepository } from "../repositories/blog-repository"
+import { ObjectId } from "mongodb"
 
 export const postValidator =  [
     body("title").exists().isString().notEmpty().trim().isLength({min: 3,max: 30}).withMessage("invalid title"),
     body("shortDescription").trim().notEmpty().isString().isLength({min: 3,max: 100}).withMessage("invalid short description"),
     body("content").trim().notEmpty().isString().isLength({min: 3,max: 1000}).withMessage("invalid content"),
     body("blogId").exists().trim().isString().notEmpty().withMessage("required valid blogId"),
-    body("blogId").custom( async (val) => await blogsRepository.findBlogById(val))
+    body("blogId").custom(  (val) => ObjectId.isValid(val))
     
 ]
 
