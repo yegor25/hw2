2e1ba444f6498b0a2cbdd08e72d606239e75283a
yegor25 <lesnichij94@mail.ru>
2023-10-20 12:23:09 +0300

    comment sthjvjhvbjhjjwdfwqfwewqdqwdfwqasa

--- dist/repositories/query/query-commentsRepository.js
+++ dist/repositories/query/query-commentsRepository.js
@@ -38,7 +38,7 @@ exports.QueryCommentsRepository = {
                 .toArray();
             const totalCount = yield db_1.commentsCollection.countDocuments({});
             return {
-                pagesCount: Math.ceil(totalCount / +parametres.pageSize) - 1,
+                pagesCount: Math.ceil(totalCount / +parametres.pageSize),
                 page: +parametres.pageNumber,
                 pageSize: +parametres.pageSize,
                 totalCount: totalCount - 1,
--- src/repositories/query/query-commentsRepository.ts
+++ src/repositories/query/query-commentsRepository.ts
@@ -29,7 +29,7 @@ export const QueryCommentsRepository = {
             .toArray()
             const totalCount = await commentsCollection.countDocuments({})
             return {
-                pagesCount:Math.ceil(totalCount/+parametres.pageSize) - 1,
+                pagesCount:Math.ceil(totalCount/+parametres.pageSize),
                 page: +parametres.pageNumber,
                 pageSize: +parametres.pageSize,
                 totalCount: totalCount - 1,
