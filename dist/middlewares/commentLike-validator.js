42e69d731ca3a1d86debf1db6444d4c4f53bb298
yegor25 <lesnichij94@mail.ru>
2023-11-29 22:56:34 +0300

    staticdsds

--- dist/middlewares/commentLike-validator.js
+++ dist/middlewares/commentLike-validator.js
@@ -5,8 +5,8 @@ const express_validator_1 = require("express-validator");
 const like_type_1 = require("../types/like-type");
 exports.commentLikeValidator = [
     (0, express_validator_1.body)("likeStatus").custom((val) => {
-        const values = Object.keys(like_type_1.LikeStatus);
-        if (!values.includes(val))
+        const values = Object.values(like_type_1.LikeStatus);
+        if (!values.some(val))
             throw new Error("exist");
     }).withMessage("unknown value")
 ];
--- src/middlewares/commentLike-validator.ts
+++ src/middlewares/commentLike-validator.ts
@@ -4,8 +4,8 @@ import { LikeStatus } from "../types/like-type";
 
 export const commentLikeValidator = [
     body("likeStatus").custom((val: any) => {
-        const values = Object.keys(LikeStatus)
-        if(!values.includes(val)) throw new Error("exist")
+        const values = Object.values(LikeStatus)
+        if(!values.some(val)) throw new Error("exist")
     }).withMessage("unknown value")
 
 ]
